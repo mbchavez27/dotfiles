5bc666c6657d36eda589cc3c7557fb5bc9453197
Max Chavez <47322039+mbchavez27@users.noreply.github.com>
2022-07-01 00:30:39 +0800

    Add config files for powershell

---
+++ config/powershell/user_profile.ps1
@@ -0,0 +1,7 @@
+#Alias
+Set-Alias vim nvim
+Set-Alias ll ls 
+Set-Alias g git 
+Set-Alias greo findstr 
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
