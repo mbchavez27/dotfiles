74a378f51f7beeffa9c13466bf103d887a27ac22
Max Chavez <47322039+mbchavez27@users.noreply.github.com>
2022-07-22 13:42:34 +0800

    Add configs.lua and plugins.nvim
    configs.lua contain the sets for neovim and configs for installed plugins while the plugins.nvim has the current installed plugins

---
+++ config/nvim/configs.lua
@@ -0,0 +1,334 @@
+-- = = = = = =   = = = =  =       =
+-- =   = =   =   =     =    =   =
+-- =   = =   =   = = = =      = 
+-- =         =   =     =    =   =
+-- =         =   =     =  =       =
+
+--Vim Configs
+local set = vim.opt 
+
+set.number = true
+set.relativenumber = true
+set.mouse = "a"
+set.clipboard = "unnamedplus"
+set.background = "dark"
+set.termguicolors = true
+set.completeopt = {'menu', 'menuone', 'noselect'}
+
+
+--Plugins Configs
+--Start Screen
+require'alpha'.setup(require'alpha.themes.startify'.config)
+
+--File Explorer
+require("nvim-tree").setup({
+    view = {
+        side = 'right'
+    }
+    }
+)
+
+--Colorizer
+require'colorizer'.setup()
+
+--Color Picker
+require("color-picker").setup({ -- for changing icons & mappings
+	-- ["icons"] = { "ﱢ", "" },
+	-- ["icons"] = { "ﮊ", "" },
+	-- ["icons"] = { "", "ﰕ" },
+	-- ["icons"] = { "", "" },
+	-- ["icons"] = { "", "" },
+	["icons"] = { "ﱢ", "" },
+	["border"] = "rounded", -- none | single | double | rounded | solid | shadow
+	["keymap"] = { -- mapping example:
+		["U"] = "<Plug>ColorPickerSlider5Decrease",
+		["O"] = "<Plug>ColorPickerSlider5Increase",
+	},
+})
+vim.cmd([[hi FloatBorder guibg=NONE]])
+--Comment
+require('Comment').setup()
+
+--Buffers
+require("bufferline").setup{}
+
+--Status Line 
+require('lualine').setup()
+options = { theme = 'gruvbox' }
+
+--Highlight Words and Lines 
+require('nvim-cursorline').setup {
+  cursorline = {
+    enable = true,
+    timeout = 0,
+    number = false,
+  },
+  cursorword = {
+    enable = true,
+    min_length = 3,
+    hl = { underline = true },
+  }
+}
+
+
+--TreeSitter
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = false,
+  }
+
+}
+
+--LSP Config
+local lsp_defaults = {
+  flags = {
+    debounce_text_changes = 150,
+  },
+  capabilities = require('cmp_nvim_lsp').update_capabilities(
+    vim.lsp.protocol.make_client_capabilities()
+  ),
+  on_attach = function(client, bufnr)
+    vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
+  end
+}
+
+local lspconfig = require('lspconfig')
+
+lspconfig.util.default_config = vim.tbl_deep_extend(
+  'force',
+  lspconfig.util.default_config,
+  lsp_defaults
+)
+
+--Servers
+lspconfig.tsserver.setup({
+ autostart = true,
+ on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+    end,
+})
+lspconfig.pyright.setup({})
+lspconfig.emmet_ls.setup({})
+lspconfig.rust_analyzer.setup({
+ autostart = true,
+ on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+    end,
+})
+
+--KeyBinds
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LspAttached',
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = true}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Displays hover information about the symbol under the cursor
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+    -- Jump to the definition
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+    -- Jump to declaration
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+    -- Lists all the implementations for the symbol under the cursor
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+    -- Jumps to the definition of the type symbol
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+    -- Lists all the references 
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+    -- Displays a function's signature information
+    bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+    -- Renames all references to the symbol under the cursor
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+
+    -- Show diagnostics in a floating window
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+    -- Move to the previous diagnostic
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+    -- Move to the next diagnostic
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+  end
+})
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+snippet = {
+  expand = function(args)
+    luasnip.lsp_expand(args.body)
+  end
+},
+
+sources = {
+  {name = 'path'},
+  {name = 'nvim_lsp'},
+  {name = 'buffer'},
+  {name = 'luasnip'},
+},
+
+window = {
+  documentation = cmp.config.window.bordered()
+},
+
+formatting = {
+	  fields = {'menu', 'abbr', 'kind'},
+  format = function(entry, item)
+    local menu_icon = {
+      nvim_lsp = 'λ',
+      luasnip = '⋗',
+      buffer = 'Ω',
+      path = '🖫',
+    }
+
+    item.menu = menu_icon[entry.source.name]
+    return item
+  end,
+ },
+ mapping = {
+  ['<CR>'] = cmp.mapping.confirm({select = true}),
+  ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+['<C-u>'] = cmp.mapping.scroll_docs(-4),
+['<C-f>'] = cmp.mapping.scroll_docs(4),
+['<C-e>'] = cmp.mapping.abort(),
+
+['<C-d>'] = cmp.mapping(function(fallback)
+  if luasnip.jumpable(1) then
+    luasnip.jump(1)
+  else
+    fallback()
+  end
+end, {'i', 's'}),
+
+['<C-b>'] = cmp.mapping(function(fallback)
+  if luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end, {'i', 's'}),
+
+['<Tab>'] = cmp.mapping(function(fallback)
+  local col = vim.fn.col('.') - 1
+
+  if cmp.visible() then
+    cmp.select_next_item(select_opts)
+  elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+    fallback()
+  else
+    cmp.complete()
+  end
+end, {'i', 's'}),
+
+['<S-Tab>'] = cmp.mapping(function(fallback)
+  if cmp.visible() then
+    cmp.select_prev_item(select_opts)
+  else
+    fallback()
+  end
+end, {'i', 's'}),
+}
+})
+
+--CMP Comamand Line 
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+--Diagnostics
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = '✘'})
+sign({name = 'DiagnosticSignWarn', text = '▲'})
+sign({name = 'DiagnosticSignHint', text = '⚑'})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+ virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+--Windows with Borders
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
+
+--Error Tabs
+require("trouble").setup {}
+
+--Format on Save
+require("null-ls").setup({
+    sources = {
+        require("null-ls").builtins.formatting.eslint_d,
+        require("null-ls").builtins.formatting.prettierd,
+        require("null-ls").builtins.formatting.black,
+	require("null-ls").builtins.formatting.rustfmt,
+    },
+})
+
+--Terminal 
+require("toggleterm").setup{}
+
+--Git
+require('vgit').setup()
---
+++ config/nvim/plugins.vim
@@ -0,0 +1,51 @@
+call plug#begin('~/AppData/Local/nvim/plugged')
+"Home Page
+Plug 'goolord/alpha-nvim'
+"Icons 
+Plug 'kyazdani42/nvim-web-devicons'
+"Buffers and Status Lines 
+Plug 'akinsho/bufferline.nvim', { 'tag': 'v2.*' }
+Plug 'nvim-lualine/lualine.nvim'
+"Color Scheme
+Plug 'ellisonleao/gruvbox.nvim'
+Plug 'tiagovla/tokyodark.nvim'
+Plug 'dikiaap/minimalist'
+"Colors
+Plug 'ziontee113/color-picker.nvim'
+"File Explorer 
+Plug 'kyazdani42/nvim-tree.lua'
+"Fuzzy Finder 
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.0' }
+"Visualize Colors
+Plug 'norcalli/nvim-colorizer.lua'
+"Discord Prescence 
+Plug 'andweeb/presence.nvim'
+"Comments Lines
+Plug 'numToStr/Comment.nvim'
+"Highlights Words and Lines 
+Plug 'yamatsum/nvim-cursorline'
+"TreeSitters
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+"Errors 
+Plug 'folke/trouble.nvim'
+"LSP and Auto Complete
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'rafamadriz/friendly-snippets'
+"Format on Save 
+Plug 'jose-elias-alvarez/null-ls.nvim'
+"Markdown 
+Plug 'iamcco/markdown-preview.nvim'
+"Terminal
+Plug 'akinsho/toggleterm.nvim', {'tag' : 'v2.*'}
+"Git
+Plug 'kdheepak/lazygit.nvim'
+Plug 'tanvirtin/vgit.nvim'
+call plug#end()
